feeff012792e38191bc6269a5116fd51e5734db8
raullexe <raullexe1@gmail.com>
2023-11-13 02:53:32 +0400

    some changes

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -41,16 +41,7 @@ $(function() {
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Errore: Non posso inviare il messaggio in questo momento. Riprova più tardi."));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
+        
         complete: function() {
           setTimeout(function() {
             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
